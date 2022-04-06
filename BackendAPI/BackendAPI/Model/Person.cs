31dc6a2894b1dec4d336f446dbb1acb6fd8df49d
Andr9651 <holmi1@live.dk>
2022-04-06 15:07:26 +0200

    Made a small minimal api for getting persons from hildur.ucn.dk

---
+++ BackendAPI/BackendAPI/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendAPI.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
--- BackendAPI/BackendAPI/Program.cs
+++ BackendAPI/BackendAPI/Program.cs
@@ -1,3 +1,11 @@
+
+
+
+
+using BackendAPI.Model;
+using Dapper;
+using System.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 // Little comment.
 // Add services to the container.
@@ -12,6 +20,22 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+
+
+app.MapGet("/Persons", () =>
+ {
+     string connectionString = "data Source=hildur.ucn.dk; Database=; User Id=dmaa0221_1089445;Password=Password1!;";
+
+     List<Person>? persons = null;
+
+     string sqlQuery = "select * from persons";
+
+     using (SqlConnection connection = new SqlConnection(connectionString))
+     {
+         persons = connection.Query<Person>(sqlQuery).ToList();
+     }
+
+     return persons;
+ });
 
 app.Run();
