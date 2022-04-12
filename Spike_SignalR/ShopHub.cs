0c57f72071204f8844d3df3d33d444f76ecbd695
Andr9651 <holmi1@live.dk>
2022-04-12 12:14:03 +0200

    Added SignalR WebClient

--- Spike_SignalR/ShopHub.cs
+++ Spike_SignalR/ShopHub.cs
@@ -5,15 +5,21 @@ namespace Spike_SignalR;
 public class ShopHub : Hub
 {
 
-    public async Task BuyBuilding(String building)
+    public async Task<string> BuyBuilding(String building)
     {
         //Check if we can buy the building
 
         //buy the building and change the game state to reflect that
+        string response = $"The {building} was bought";
 
+
+        Console.WriteLine($"=====================\nUser tried to buy a {building}\n==================");
 
         //announce to all players how the game state has changed
-        await Clients.All.SendAsync("BuyBuildingResponse", $"The {building} was bought");
+
+        await Clients.All.SendAsync("BuyBuildingResponse", response);
+
+        return response;
     }
 
 }
