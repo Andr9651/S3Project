e56207f44a7b554043f8e561869f36d2cc32bddc
Pepega68 <1054690@ucn.dk>
2022-04-21 10:43:46 +0200

    Created new GameHost project. Added Model class Added Controller class ¯\_(ツ)_/¯ AND A WHOLE TEST PROJECT

---
+++ DesktopHostingClient/DesktopHostingClient/Controller/GameDataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopHostingClient.Model;
+
+namespace DesktopHostingClient.Controller;
+
+public class GameDataController
+{
+    public GameData? GameData { get; private set; }
+    public void CreateGameData()
+    {
+        GameData = new GameData();
+    }
+    
+}
---
+++ DesktopHostingClient/DesktopHostingClient/Model/GameData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopHostingClient.Model
+{
+    public class GameData
+    {
+        public int Id { get; set; }
+        public int Balance { get; set; }
+        public string IP { get; set; }
+        public Dictionary<int,int> Purchases { get; set; }
+
+        public GameData()
+        {
+            Purchases = new Dictionary<int, int>();
+        }
+    }
+}
