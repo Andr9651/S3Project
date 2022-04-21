f51dda7c415739a81120e726d67632fa098170d2
Sylvest96 <mikkelsylvestrasmussen@outlook.dk>
2022-04-21 15:14:13 +0200

    Made GameDataController a singleton

--- DesktopHostingClient/DesktopHostingClient/Controller/GameDataController.cs
+++ DesktopHostingClient/DesktopHostingClient/Controller/GameDataController.cs
@@ -10,9 +10,26 @@ namespace DesktopHostingClient.Controller;
 public class GameDataController
 {
     public GameData? GameData { get; private set; }
+    private static GameDataController _instance;
+
+    private GameDataController()
+    {
+
+    }
+
+    public static GameDataController GetInstance()
+    {
+        if (_instance is null)
+        {
+            _instance = new GameDataController();
+        }
+
+        return _instance;
+    }
+
     public void CreateGameData()
     {
         GameData = new GameData();
     }
-    
+
 }
