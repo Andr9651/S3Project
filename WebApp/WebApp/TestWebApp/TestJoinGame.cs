1d36b5f059b7cd60fc3eb24950094fb579afc153
Pepega68 <1054690@ucn.dk>
2022-04-26 10:57:30 +0200

    Impemented test method for "CreateJoinGameURI"

--- WebApp/WebApp/TestWebApp/TestJoinGame.cs
+++ WebApp/WebApp/TestWebApp/TestJoinGame.cs
@@ -9,13 +9,24 @@ public class TestJoinGame
     [InlineData("600.900.-1.000:8", false)]
     [InlineData("600.900.120.000:8080", false)]
     [InlineData("192.168.1.15", false)]
-
     public void TestValidateIP(string ip, bool expected)
     {
         //Arrange
 
         //Act
         bool result = JoinGameManager.ValidateIP(ip);
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("192.168.1.15:8080", "/Game/192.168.1.15:8080")]
+    [InlineData("Foobar", "/Game/Foobar")]
+    public void TestCreateJoinGameURI(string ip, string expected)
+    {
+        //Arrange
+        //Act
+        string result = JoinGameManager.CreateJoinGameURI(ip);
         //Assert
         Assert.Equal(expected, result);
     }
