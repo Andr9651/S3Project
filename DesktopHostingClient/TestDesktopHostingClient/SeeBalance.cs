5f2df0a7b98f37df5008c61343794ba026bd1ed1
Sylvest96 <mikkelsylvestrasmussen@outlook.dk>
2022-05-02 12:38:42 +0200

    Finished TestReciveBalanceUpdate

--- DesktopHostingClient/TestDesktopHostingClient/SeeBalance.cs
+++ DesktopHostingClient/TestDesktopHostingClient/SeeBalance.cs
@@ -6,25 +6,13 @@ using System.Threading.Tasks;
 using Xunit;
 using DesktopHostingClient.Managers;
 using Microsoft.AspNetCore.SignalR.Client;
- 
+using System.Threading;
 namespace TestDesktopHostingClient;
 
 public class SeeBalance
 {
-
-    public void HandleBalanceUpdate (int balance)
-    {
-        console
-
-    }
-
-
-
-
-
     [Fact]
-
-    public void TestReciveBalanceUpdate ()
+    public void TestReciveBalanceUpdate()
     {
         // Arrange 
         GameDataManager gameDataManager = GameDataManager.GetInstance();
@@ -32,26 +20,21 @@ public class SeeBalance
         HostingManager hostingManager = new HostingManager();
         hostingManager.SetupSignalRHost();
         hostingManager.StartHosting().Wait();
+
         // Act 
         IHubConnectionBuilder hubConnectionBuilder = new HubConnectionBuilder();
         hubConnectionBuilder.WithUrl("http://localhost:5100/GameHub");
         HubConnection connection = hubConnectionBuilder.Build();
-        bool reciveUpdate = false;
-        //connection.On("BalanceUpdate", (balance) =>
-        //{
-        //    reciveUpdate = true;
-        //});
+        bool receivedUpdate = false;
+        connection.On<int>("BalanceUpdate", (balance) =>
+        {
+            receivedUpdate = true;
+        });
+        connection.StartAsync().Wait();
+        gameDataManager.NotifyBalanceChanged();
+        Thread.Sleep(500);
 
-        // Assert
-
-        
-
-
-
+        //Assert
+        Assert.True(receivedUpdate);
     }
-
-
-
-
-
 }
