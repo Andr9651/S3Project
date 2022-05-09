24e9e3677bdf3a7cf0b7a9f1ff9c96ae44abdc88
Andr9651 <holmi1@live.dk>
2022-05-09 12:05:38 +0200

    Added SaveGameInstance to SQLGameDataService (Missing SQL Query)

--- BackendAPI/BackendAPI/Service/SQLGameDataService.cs
+++ BackendAPI/BackendAPI/Service/SQLGameDataService.cs
@@ -6,16 +6,15 @@ namespace BackendAPI.Service;
 
 public class SQLGameDataService
 {
+    private string _dbConnectionString = "Data Source=.;Initial Catalog=CookieClicker;Integrated Security=True";
 
     public List<PurchasableDto> GetPurchasables()
     {
-        string connectionString = "Data Source=.;Initial Catalog=CookieClicker;Integrated Security=True";
-
         List<PurchasableDto>? purchasableDtos = null;
 
         string sqlQuery = "select * from Purchasable";
 
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(_dbConnectionString))
         {
             purchasableDtos = connection.Query<PurchasableDto>(sqlQuery).ToList();
         }
@@ -23,5 +22,60 @@ public class SQLGameDataService
         return purchasableDtos;
     }
 
+    public bool SaveGameInstance(GameInstance gameInstance)
+    {
+        bool result = false;
+
+        string sqlQueryUpdateGameInstance = "";
+        string sqlQueryUpdateGamePurchases = "";
+
+        GameInstanceDto gameInstanceDto = gameInstance.GetGameInstanceDto();
+
+        List<GamePurchaseDto> gamePurchaseDtoList = gameInstance.GetGamePurchaseDtos();
+
+        using (SqlConnection connection = new SqlConnection(_dbConnectionString))
+        {
+            using (SqlTransaction transaction = connection.BeginTransaction())
+            {
+                bool error = false;
+
+                int linesChangedGameInstance = connection.Execute(
+                    sqlQueryUpdateGameInstance,
+                    gameInstanceDto
+                );
+
+                if (linesChangedGameInstance == 0)
+                {
+                    error = true;
+                }
+
+                if (!error && gamePurchaseDtoList.Count > 0)
+                {
+                    int linesChangedGamePurchases = connection.Execute(
+                        sqlQueryUpdateGamePurchases,
+                        gamePurchaseDtoList
+                    );
+
+                    if (linesChangedGamePurchases == 0)
+                    {
+                        error = true;
+                    }
+                }
+
+                if (error)
+                {
+                    transaction.Rollback();
+                    result = false;
+                }
+                else
+                {
+                    transaction.Commit();
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
 }
 
