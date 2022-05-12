012a7f1591878b404d7eeaa513d41893903ca466
Andr9651 <holmi1@live.dk>
2022-05-12 15:18:28 +0200

    Created ClassLibrary

---
+++ ModelLibrary/Model/Purchasable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Model;
+public class Purchasable
+{
+    public int Id { get; set; }
+    public int Price { get; set; }
+    public string Name { get; set; }
+}
+
