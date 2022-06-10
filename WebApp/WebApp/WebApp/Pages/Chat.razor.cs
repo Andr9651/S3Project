6b77910b134647b19df4cce29509c5d97841a440
Andr9651 <holmi1@live.dk>
2022-06-10 08:18:30 +0200

    Stated implementing Chatbox UI

---
+++ WebApp/WebApp/WebApp/Pages/Chat.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebApp.Managers;
+
+namespace WebApp.Pages
+{
+    public partial class Chat : ComponentBase
+    {
+        [Parameter]
+        public GameManager GameManager { get; init; }
+
+
+
+    }
+}
